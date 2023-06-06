3d81bfee4488ab0e0ce57041b00ddc2541e19216
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 17:30:14 +0100

    Put setComponentsNames code in a different file

--- src/index.js
+++ src/index.js
@@ -2,19 +2,3 @@ import fs from "fs";
 import create from "prompt-sync";
 
 export const input = create();
-
-function setComponentsNames() {
-  const componentsNames = input(
-    "Choose names for your components (put space between each one): "
-  ).split(" ");
-
-  const capitalizedComponentsNames = componentsNames.map(capitalize);
-
-  return capitalizedComponentsNames;
-}
-
-function capitalize(word) {
-  word = word.charAt(0).toUpperCase() + word.slice(1);
-
-  return word;
-}
---
+++ src/utils/setComponentsNames.js
@@ -0,0 +1,19 @@
+import { input } from "../index.js";
+
+function setComponentsNames() {
+  const componentsNames = input(
+    "Choose names for your components (put space between each one): "
+  ).split(" ");
+
+  const capitalizedComponentsNames = componentsNames.map(capitalize);
+
+  return capitalizedComponentsNames;
+}
+
+function capitalize(word) {
+  word = word.charAt(0).toUpperCase() + word.slice(1);
+
+  return word;
+}
+
+export default setComponentsNames;
