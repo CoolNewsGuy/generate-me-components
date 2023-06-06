a627a96459b0014ae94657f057e0a57f8439eb46
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 20:00:12 +0100

    Add a function to let user choose his preferred stylesheet language

---
+++ src/utils/chooseStyleSheetLanguage.js
@@ -0,0 +1,11 @@
+import { input } from "../index.js";
+
+function chooseStyleSheetLanguage() {
+  const language = input(
+    "What StyleSheet Language you're using (e.g., CSS, SCSS, SASS...): "
+  );
+
+  return language.toUpperCase();
+}
+
+export default chooseStyleSheetLanguage;
