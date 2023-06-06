c8030ea766a66bdc86cf6562fc3c25ec6b696677
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 23:40:58 +0100

    Create a function that will return a boilerplate for component script file

--- src/utils/createComponentFiles.js
+++ src/utils/createComponentFiles.js
@@ -17,4 +17,19 @@ function createComponentFiles(componentFolderName) {
   componentFiles.forEach((file) => writeFileSync(file, ""));
 }
 
+function writeToComponentScriptFile(componentFiles, componentFolderName) {
+  const boilerplate = `import './${componentFiles[0]}'
+
+function ${componentFolderName}() {
+  return (
+    <></>
+  )
+}
+      
+export default ${componentFolderName};
+`;
+
+  return boilerplate;
+}
+
 export default createComponentFiles;
