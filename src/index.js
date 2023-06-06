e0aa6dcd5b78dabe8b75da2c1fd75dd65a83c1c6
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 22:58:06 +0100

    Store the chosen stylesheet language in a variable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import create from "prompt-sync";
 import createComponentsDir from "./utils/createComponentsDir.js";
 import createComponentsFolders from "./utils/createComponentsFolders.js";
+import chooseStyleSheetLanguage from "./utils/chooseStyleSheetLanguage.js";
 
 export const input = create();
 
 export const componentsPath = createComponentsDir();
 export const componentsFolders = createComponentsFolders();
+export const chosenStyleSheetLanguage = chooseStyleSheetLanguage();
