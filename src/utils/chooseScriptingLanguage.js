4be226883641cf8c26b62ec3f2ee846d3d38ffe3
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 23:10:45 +0100

    Make the chooseScriptingLanguage function return language name in lower case

--- src/utils/chooseScriptingLanguage.js
+++ src/utils/chooseScriptingLanguage.js
@@ -3,7 +3,7 @@ import { input } from "../index.js";
 function chooseScriptingLanguage() {
   const language = input("What Scripting Language you're using (JSX or TSX): ");
 
-  return language.toUpperCase();
+  return language.toLowerCase();
 }
 
 export default chooseScriptingLanguage;
