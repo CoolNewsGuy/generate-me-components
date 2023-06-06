bf2401857620f3fea06076c50d06d2eacfd20560
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 23:20:34 +0100

    Use the createComponentFiles function to create each component's files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import createComponentsDir from "./utils/createComponentsDir.js";
 import createComponentsFolders from "./utils/createComponentsFolders.js";
 import chooseStyleSheetLanguage from "./utils/chooseStyleSheetLanguage.js";
 import chooseScriptingLanguage from "./utils/chooseScriptingLanguage.js";
+import createComponentFiles from "./utils/createComponentFiles.js";
 
 export const input = create();
 
@@ -10,3 +11,5 @@ export const componentsPath = createComponentsDir();
 export const componentsFolders = createComponentsFolders();
 export const chosenStyleSheetLanguage = chooseStyleSheetLanguage();
 export const chosenScriptingLanguage = chooseScriptingLanguage();
+
+componentsFolders.forEach(createComponentFiles);
