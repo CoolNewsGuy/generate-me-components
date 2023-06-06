3cfac0cd002f074b9446906f2027fe1e006fec4a
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-06 21:30:32 +0100

    Store the chosen components directory in a variable and export it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import create from "prompt-sync";
+import createComponentsDir from "./utils/createComponentsDir.js";
 
 export const input = create();
+
+export let componentsPath = createComponentsDir();
