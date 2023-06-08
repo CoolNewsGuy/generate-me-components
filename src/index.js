9c6507ec5fd613fc0add803c04ae0fd95b7dfa6c
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-08 11:20:19 +0100

    Move index.js to src

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 #! /usr/bin/env node
 
 import create from "prompt-sync";
-import createComponentsDir from "../src/createFunctions/createComponentsDir.js";
-import createComponentsFolders from "../src/createFunctions/createComponentsFolders.js";
-import chooseStyleSheetLanguage from "../src/chooseFunctions/chooseStyleSheetLanguage.js";
-import chooseScriptingLanguage from "../src/chooseFunctions/chooseScriptingLanguage.js";
-import createComponentFiles from "../src/createFunctions/createComponentFiles.js";
+import createComponentsDir from "./createFunctions/createComponentsDir.js";
+import createComponentsFolders from "./createFunctions/createComponentsFolders.js";
+import chooseStyleSheetLanguage from "./chooseFunctions/chooseStyleSheetLanguage.js";
+import chooseScriptingLanguage from "./chooseFunctions/chooseScriptingLanguage.js";
+import createComponentFiles from "./createFunctions/createComponentFiles.js";
 import createConfigFileIfPermitted, {
   shouldCreateConfigFile,
-} from "../src/createFunctions/createConfigFileIfPermitted.js";
+} from "./createFunctions/createConfigFileIfPermitted.js";
 import { existsSync } from "fs";
-import checkIfConfigFileIsValid from "../src/checkFunctions/checkIfConfigFileIsValid.js";
-import readConfigFile from "../src/readFunctions/readConfigFile.js";
+import checkIfConfigFileIsValid from "./checkFunctions/checkIfConfigFileIsValid.js";
+import readConfigFile from "./readFunctions/readConfigFile.js";
 import figlet from "figlet";
 
 console.log(
