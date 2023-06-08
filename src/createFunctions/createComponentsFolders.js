af12e61caa4b4d5986a33f9ad9f24941d044e717
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-08 17:22:41 +0100

    Refactor createComponentsFolders function

--- src/createFunctions/createComponentsFolders.js
+++ src/createFunctions/createComponentsFolders.js
@@ -1,20 +1,15 @@
 import { existsSync, mkdirSync } from "fs";
-import chooseComponentsNames from "../chooseFunctions/chooseComponentsNames.js";
 import createComponentsDir from "./createComponentsDir.js";
-import { componentsPath } from "../index.js";
+import { componentsFolders, componentsPath } from "../index.js";
 
 function createComponentsFolders() {
-  const componentsNames = chooseComponentsNames();
-
   if (!existsSync(componentsPath)) {
     createComponentsDir();
   }
 
-  componentsNames.forEach((componentName) => {
-    mkdirSync(`${componentsPath}/${componentName}`);
+  componentsFolders.forEach((componentsFolder) => {
+    mkdirSync(`${componentsPath}/${componentsFolder}`);
   });
-
-  return componentsNames;
 }
 
 export default createComponentsFolders;
