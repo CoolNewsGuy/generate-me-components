5182241e9d8d696ada7e5cd898056764a6cbc744
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-08 06:37:12 +0100

    Put setGeneratedComponentsDir function in a different file

--- src/createFunctions/createComponentsDir.js
+++ src/createFunctions/createComponentsDir.js
@@ -1,5 +1,5 @@
 import { mkdirSync } from "fs";
-import { input } from "../../bin/index.js";
+import setGeneratedComponentsDir from "../setFunctions/setGeneratedComponentsDir.js";
 
 function createComponentsDir() {
   const chosenDir = setGeneratedComponentsDir();
@@ -7,20 +7,6 @@ function createComponentsDir() {
   mkdirSync(chosenDir, { recursive: true });
 
   return chosenDir;
-}
-
-function setGeneratedComponentsDir() {
-  const currentDir = process.cwd();
-  let componentsFolder = input("Where to put the generated components: ");
-
-  if (componentsFolder.startsWith("/")) {
-    componentsFolder = componentsFolder.slice(1);
-  }
-  if (componentsFolder.startsWith("./")) {
-    componentsFolder = componentsFolder.slice(2);
-  }
-
-  return `${currentDir}/${componentsFolder}`;
 }
 
 export default createComponentsDir;
