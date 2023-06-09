3d569e74acfb3bc770576c1d58e4579547b8edf6
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-09 05:14:02 +0100

    Add a default value of should create config file prompt

--- src/shouldFunctions/shouldCreateConfigFile.js
+++ src/shouldFunctions/shouldCreateConfigFile.js
@@ -5,7 +5,8 @@ function shouldCreateConfigFile() {
   const shouldCreateFile = input(
     chalk.cyanBright(
       "Would you like to create a config file to quickly create components? (Y/n): "
-    )
+    ),
+    "yes"
   );
 
   return ["Y", "y", "yes", "Yes"].includes(shouldCreateFile);
