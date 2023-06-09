f9b68bdf9a217fb82c91a791eb9afd87a6a3f033
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-09 05:11:51 +0100

    Move shouldCreateConfigFile function to a new file

--- src/createFunctions/createConfigFileIfPermitted.js
+++ src/createFunctions/createConfigFileIfPermitted.js
@@ -1,6 +1,5 @@
-import { input, isConfigFileAllowed } from "../index.js";
+import { isConfigFileAllowed } from "../index.js";
 import writeConfigFile from "../writeFunctions/writeConfigFile.js";
-import chalk from "chalk";
 
 function createConfigFileIfPermitted() {
   if (isConfigFileAllowed) {
@@ -8,15 +7,4 @@ function createConfigFileIfPermitted() {
   }
 }
 
-function shouldCreateConfigFile() {
-  const shouldCreateFile = input(
-    chalk.cyanBright(
-      "Would you like to create a config file to quickly create components? (Y/n): "
-    )
-  );
-
-  return ["Y", "y", "yes", "Yes"].includes(shouldCreateFile);
-}
-
-export { shouldCreateConfigFile };
 export default createConfigFileIfPermitted;
---
+++ src/shouldFunctions/shouldCreateConfigFile.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+import { input } from "../index.js";
+
+function shouldCreateConfigFile() {
+  const shouldCreateFile = input(
+    chalk.cyanBright(
+      "Would you like to create a config file to quickly create components? (Y/n): "
+    )
+  );
+
+  return ["Y", "y", "yes", "Yes"].includes(shouldCreateFile);
+}
+
+export default shouldCreateConfigFile;
