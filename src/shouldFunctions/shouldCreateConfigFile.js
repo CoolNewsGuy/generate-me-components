3fa53b98b056d1fabbf419f2359be517083887cf
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-09 05:15:42 +0100

    Change should create config file prompt

--- src/shouldFunctions/shouldCreateConfigFile.js
+++ src/shouldFunctions/shouldCreateConfigFile.js
@@ -4,7 +4,7 @@ import { input } from "../index.js";
 function shouldCreateConfigFile() {
   const shouldCreateFile = input(
     chalk.cyanBright(
-      "Would you like to create a config file to quickly create components? (Y/n): "
+      "Would you like to create a config file to quickly create components? (y/n) | default = yes: "
     ),
     "yes"
   );
