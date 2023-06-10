f1811ae08e34803bd750b0d3a52463fe617f27e4
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-10 21:59:46 +0100

    Change config file name in readConfigFIle function

--- src/readFunctions/readConfigFile.js
+++ src/readFunctions/readConfigFile.js
@@ -2,14 +2,14 @@ import { readFileSync } from "fs";
 
 function readConfigFile() {
   try {
-    const content = readFileSync("gift-me-components.json");
+    const content = readFileSync("generate-me-components.json");
 
     const contentToObj = JSON.parse(content);
 
     return contentToObj;
   } catch {
     throw new Error(
-      "gift-me-components.json is not a valid json file. Make sure to add {} to it if it's empty or delete it."
+      "generate-me-components.json is not a valid json file. Make sure to add {} to it if it's empty or delete it."
     );
   }
 }
