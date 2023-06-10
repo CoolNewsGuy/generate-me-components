22196e617f2ce56e3a934f7c9940b2aa2e66e4c7
Unknown Cat <joewilliamdd31@gmail.com>
2023-06-10 05:40:35 +0100

    Throw an error if config file is empty or without {}

--- src/readFunctions/readConfigFile.js
+++ src/readFunctions/readConfigFile.js
@@ -1,11 +1,17 @@
 import { readFileSync } from "fs";
 
 function readConfigFile() {
-  const content = readFileSync("gift-me-components.json");
+  try {
+    const content = readFileSync("gift-me-components.json");
 
-  const contentToObj = JSON.parse(content);
+    const contentToObj = JSON.parse(content);
 
-  return contentToObj;
+    return contentToObj;
+  } catch {
+    throw new Error(
+      "gift-me-components.json is not a valid json file. Make sure to add {} to it if it's empty or delete it."
+    );
+  }
 }
 
 export default readConfigFile;
